a0ad66771c19177c749b5cf5e66db5e27c51058f
suanmei <code_lee@sina.com>
2019-10-31 11:32:41 +0800

    feat: umi-in-action 信息更改

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Umi in action`,
+    siteUrl: `https://umi.notodo.cn`,
+    description: `UmiJs 实战`,
+    author: `authored by 拾邑@suanmei, 罄天@liangklfangl, 十忆@zhangyuang; reviewed by 狼叔@i5ting`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Umi in action`,
+        short_name: `Umi in action`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/umi-in-action-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
