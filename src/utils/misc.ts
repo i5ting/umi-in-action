6964196860ad908df024828ec4751f7a368465e6
suanmei <code_lee@sina.com>
2020-04-15 12:36:02 +0800

    feat: 文档 yml 格式更改

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,13 +1,10 @@
 import * as docList from '../../docs/docs.yml';
-import { AllMarkdown, NearPage } from '../model';
-import transArray2Map from './transform';
-
-export const getMetaMap = transArray2Map('fields.slug', 'frontmatter.title');
+import { NearPage } from '../model';
 
 export const getMdUrl = (slug: string): string => {
   const prefix = 'https://github.com/ykfe/umi-in-action/edit/master/';
 
-  if (slug === 'docs/overview') {
+  if (slug === 'docs') {
     return `${prefix}docs/README.md`;
   }
 
@@ -38,31 +35,27 @@ const getNextPage = (index: number, paths: string[], docMap: Record<string, stri
 };
 
 export const getNearPage = (
-  slug: string,
-  metaData: AllMarkdown
+  slug: string
 ): {
   prev: NearPage;
   next: NearPage;
 } => {
-  const metaMap = getMetaMap(metaData);
   const docMap: Record<string, string> = {};
-  const prefix = 'docs';
 
-  docList.forEach(doc => {
-    const { chapter, sections } = doc;
-    const chapterPath = `${prefix}/${chapter}`;
-    docMap[chapterPath] = metaMap.get(chapterPath);
+  docList.forEach(chapter => {
+    const { title, link, items } = chapter;
+    docMap[link] = title;
 
-    if (!sections) return;
+    if (!items) return;
 
-    sections.forEach(section => {
-      const sectionPath = `${prefix}/${chapter}/${section}`;
-      docMap[sectionPath] = metaMap.get(sectionPath);
+    items.forEach(section => {
+      const { title: sectionTitle, link: sectionLink } = section;
+      docMap[sectionLink] = sectionTitle;
     });
   });
 
   const paths = Object.keys(docMap);
-  const index = paths.findIndex(path => path === slug);
+  const index = paths.findIndex(path => path === `/${slug}`);
 
   return {
     prev: getPrevPage(index, paths, docMap),
